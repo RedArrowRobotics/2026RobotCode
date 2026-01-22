fb4bd4ea2e3fd2ca47f448292f77ffa57951aa54
RedArrowRobotics <79018739+RedArrowRobotics@users.noreply.github.com>
2026-01-21 19:53:11 -0500

    Fuel Thrower Stub Complete?
    I added important stuff to the top of the file.

--- src/main/java/frc/robot/subsystems/FuelThrowerSubsystem.java
+++ src/main/java/frc/robot/subsystems/FuelThrowerSubsystem.java
@@ -1,3 +1,7 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class FuelThrowerSubsystem {
 
   public Command throwerActivate() {
