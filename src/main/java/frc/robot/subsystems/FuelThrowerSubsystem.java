9d4afcb7e93e4c42607aaadb794b3c7ac6e7ba00
RedArrowRobotics <79018739+RedArrowRobotics@users.noreply.github.com>
2026-01-21 19:00:19 -0500

    Fuel Thrower Subsystem File
    I just copied another file to get it ready for editing.

---
+++ src/main/java/frc/robot/subsystems/FuelThrowerSubsystem.java
@@ -0,0 +1,53 @@
+public class FuelThrowerSubsystem {
+
+  public Command intakeIn() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  public Command intakeOut() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  public Command intakeStop() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  public Command extendIntake() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  public Command retractIntake() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  public boolean genericSensor() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+  
+}
