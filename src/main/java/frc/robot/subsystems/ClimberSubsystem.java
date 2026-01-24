23c97edd0b5a72c6fc747d223862021091d7c3a8
Jalasta <jalasta20xx@gmail.com>
2026-01-24 11:21:42 -0500

    integrate climber subsystem skeleton

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -3,33 +3,24 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimberSubsystem {
+public class ClimberSubsystem extends SubsystemBase {
 
-  public Command climberAscend() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
+    public Command climberAscend() {
+        return runOnce(() -> {
+            
         });
-  }
+    }
 
-  public Command climberStop() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
+    public Command climberStop() {
+        return runOnce(() -> {
+            
         });
-  }
+    }
 
-  public Command climberDescend() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
+    public Command climberDescend() {
+        return runOnce(() -> {
+            
         });
-  }
+    }
 
 }
