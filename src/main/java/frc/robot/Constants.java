a4911e8c622ee0adfb21136e4b9ecc1d8de96de3
FRCTeam3234 <79018739+FRCTeam3234@users.noreply.github.com>
2026-02-21 16:09:18 -0500

    Added code for the intake and hopper extension, sys id routines for the hopper extension, and SmartDashboard commands to test all of the commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,4 +20,24 @@ public final class Constants {
     public final class FuelShooterConstants {
         public static final double THROWER_POWER = 0.5;
     }
+
+    public final class IntakeConstants {
+        public static final int INTAKE_MOTOR_ID = 23;
+        public static final int HOPPER_EXTENDER_MOTOR_ID = 24;
+        public static final double INTAKE_SPEED = 0.5;
+        public static final double HOPPER_MANUAL_SPEED = 0.5;
+
+        public static final double HOPPER_kP = 0.0;
+        public static final double HOPPER_kI = 0.0;
+        public static final double HOPPER_kD = 0.0;
+        public static final double HOPPER_kV = 0.0;
+        public static final double HOPPER_kS = 0.0;
+        public static final double HOPPER_kA = 0.0;
+        public static final double HOPPER_MAX_VELOCITY = 0.0;
+        public static final double HOPPER_MAX_ACCELERATION = 0.0;
+        public static final double HOPPER_MAX_ERROR = 0.0;
+
+        public static final double HOPPER_EXTENDED_POSITION = 0.0;
+        public static final double HOPPER_RETRACTED_POSITION = 0.0;
+    }
 }
