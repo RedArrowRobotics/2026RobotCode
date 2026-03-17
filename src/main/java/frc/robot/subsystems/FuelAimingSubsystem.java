637875ef6cafab1f9adf2f96c94c517089edfd2a
CoderLiam926 <LiamVanderJagt@gmail.com>
2026-03-17 19:29:24 -0400

    Conversions

--- src/main/java/frc/robot/subsystems/FuelAimingSubsystem.java
+++ src/main/java/frc/robot/subsystems/FuelAimingSubsystem.java
@@ -133,7 +133,7 @@ public class FuelAimingSubsystem extends SubsystemBase {
 			}
 			turretRotator.set(0.0);
 			turretSetZeroStart = true;
-			turretRotator.getEncoder().setPosition(Constants.FuelAimingConstants.THROWER_ROTATION_SET);
+			turretRotator.getEncoder().setPosition(Constants.FuelAimingConstants.THROWER_ROTATION_SET * -0.2132);
     	}
 	}
 
